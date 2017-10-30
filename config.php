18b02f4a82bffc1844c05c51fa2c6b21831310c4
Bigbaddevil7 <st8ples@yahoo.com>
2017-10-30 03:57:58 -0500

    updated

--- config.php
+++ config.php
@@ -11,34 +11,4 @@ $twitchLib['codeFlow'] = "OAuthAuth"; //Code Flow type. OAuthAuth is recommened 
 $twitchLib['apiVersion'] = "V5"; // Can user 'HELIX' or 'V5'
 $twitchLib['debugMode']=false; // sets debug mode for the API to print out what its doing.
 
-
-/*
-
-SCOPES
-
-'channel_check_subscription',
-'channel_commercial',
-'channel_editor',
-'channel_feed_edit',
-'channel_feed_read',
-'channel_read',
-'channel_stream',
-'channel_subscriptions',
-'chat_login',
-'collections_edit',
-'communities_edit',
-'communities_moderate',
-'openid',
-'user_blocks_edit',
-'user_blocks_read',
-'user_follows_edit',
-'user_read',
-'user_subscriptions',
-'viewing_activity_read',
-'user:edit',
-'user:read:email'
-
-VALID CODE
-
-
 ?>
