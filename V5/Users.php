b7b71f78c4b4c698fac31f27b2726d1ed6bb4942
william staples <st8ples@yahoo.com>
2017-10-30 03:59:52 -0500

    updated

--- V5/Users.php
+++ V5/Users.php
@@ -7,7 +7,6 @@
 namespace devilLib;
 require_once('TwitchAPILib.php');
 class Users extends TwitchAPILib{
-  //$token = 'xo5x20ms9spupdhf2mlii65mk4eu40';
   public function getUser($id){
     $ch = curl_init();
 		$response = $this->execCurl($ch, self::V5URL . 'users/'.$id, $this->getStreamerOauth());
